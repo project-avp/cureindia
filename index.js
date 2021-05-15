08883db4be99e284d9e4415ab1f16c804424c2b6
project-avp <project.avp03@gmail.com>
2021-05-15 13:17:38 +0530

    Styling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+
 const path=require('path');
 
 const mongoose=require('mongoose');
@@ -15,7 +16,11 @@ mongoose.connect('mongodb://localhost:27017/cureIndiaP', {useNewUrlParser: true,
 })
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));4
+
+//Styling
+app.use(express.static('assests'));
+
 //New Entry patient
 app.get('/products/new',(req,res)=>{
     res.render('products/new')
